45be7796a7dc7cde9930d54990547a3dce856f47
Kathy28Liz <70820427+Kathy28Liz@users.noreply.github.com>
2023-06-05 12:42:22 -0500

    Commit RestTemplate Service

--- Employees/src/main/java/co/com/amaris/user/EmployeesApplication.java
+++ Employees/src/main/java/co/com/amaris/user/EmployeesApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EmployeesApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeesApplication.class, args);
 	}
